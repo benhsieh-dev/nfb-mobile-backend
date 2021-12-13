84b44599c0d3813d17b49209fde7e9439ca18722
benhsieh-dev <bqh5026@gmail.com>
2021-12-12 23:58:39 -0500

    Set cross origins for android, ios, and localhost

--- src/main/java/com/nfb/mobile/backend/dao/ProductRepository.java
+++ src/main/java/com/nfb/mobile/backend/dao/ProductRepository.java
@@ -4,6 +4,8 @@ import com.nfb.mobile.backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:8102")
+//@CrossOrigin
+@CrossOrigin(origins = {"http://localhost (Android)", "capacitor://localhost (iOS)", "http://localhost:8102"})
+//@CrossOrigin (origins = {"http://localhost:8100", "http://localhost:8101", "http://localhost:8102", "capacitor://localhost", "ionic://localhost"})
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
---
+++ src/test/java/com/nfb/mobile/backend/dao/ProductRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.nfb.mobile.backend.dao;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRepositoryTest {
+
+}
