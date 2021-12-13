776b0c8389f4d9f30f301a6df2c3c894c682c488
benhsieh-dev <bqh5026@gmail.com>
2021-12-13 01:06:55 -0500

    Edit cross origin settings

--- src/main/java/com/nfb/mobile/backend/dao/ProductCategoryRepository.java
+++ src/main/java/com/nfb/mobile/backend/dao/ProductCategoryRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 
-@CrossOrigin("http://localhost:8102")
+//@CrossOrigin("http://localhost:8102")
+@CrossOrigin
+//@CrossOrigin("http//localhost")
+//@CrossOrigin("capacitor://localhost")
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 }
--- src/main/java/com/nfb/mobile/backend/dao/ProductRepository.java
+++ src/main/java/com/nfb/mobile/backend/dao/ProductRepository.java
@@ -4,8 +4,13 @@ import com.nfb.mobile.backend.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-//@CrossOrigin
-@CrossOrigin(origins = {"http://localhost (Android)", "capacitor://localhost (iOS)", "http://localhost:8102"})
+@CrossOrigin
+//@CrossOrigin(origins = {"http://localhost:4200", "ionic://localhost", "http://localhost", "capacitor://localhost", "http://localhost:8100", "http://localhost:8101", "http://localhost:8102"})
 //@CrossOrigin (origins = {"http://localhost:8100", "http://localhost:8101", "http://localhost:8102", "capacitor://localhost", "ionic://localhost"})
+//@CrossOrigin("capacitor//localhost")
+//@CrossOrigin("http//localhost")
+//@CrossOrigin("capacitor://localhost")
+//@CrossOrigin("http//localhost:4200")
+//@CrossOrigin("http//localhost:8100")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
